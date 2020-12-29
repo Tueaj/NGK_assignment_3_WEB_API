897aef872b03ce50bb8a569ee31fcc63d1026c53
AUJacobNyvang <70698246+AUJacobNyvang@users.noreply.github.com>
2020-12-29 14:51:57 +0100

    Testprojekt

---
+++ WeatherReadingsAPI/ControllerClassUnitTest/NunitWeatherReportController.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using WeatherReadingsAPI.Controllers;
+
+namespace ControllerClassUnitTest
+{
+    [TestFixture]
+    public class NunitWeatherReportController
+    {
+        private WeatherReportController _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ WeatherReadingsAPI/UnitTestController/AccountControllerTest.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace UnitTestController
+{
+    public class Tests
+    {
+        private AccountController _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
